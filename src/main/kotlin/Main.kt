bb7ecd204f218cdec084c76e7b354480a8e3a3e6
Kaitlyn Clay <kclay@students.mchenry.edu>
2023-08-17 17:09:58 -0500

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>) {
+    println("I grew up on an organic farm about 1.5 hours west of here.")
+    println("My dad, 2 uncles, brother, and 2 cousins have grown the farm to the point that it is the 3rd largest organic seed corn producer in the US.")
+    println("I have helped on the farm my whole life. I was also homeschooled my entire life.")
+    println("I love being outside at night in the country. I love the quietness even if there are tons of crickets making a racket.")
+    println("Also  really enjoy horses, dogs, and fish. I have a horse and dog and about 30 gallons of fish.")
+
+}
